560d3ee0cf2b4487c8d896e085c351ffe96cc18f
Glenn Tarpey <glenntarpey@gmail.com>
2024-12-16 09:49:48 +0000

    Removed info

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -1,6 +1,6 @@
 import { HiOutlineMail } from "react-icons/hi";
 import { FaLinkedinIn } from "react-icons/fa"; // Using LinkedIn icon
-import { FaWhatsapp } from "react-icons/fa";
+//import { FaWhatsapp } from "react-icons/fa";
 
 const data = [
   { id: 1, icon: <HiOutlineMail />, link: "mailto:glenntarpey@gmail.com" },
@@ -9,7 +9,7 @@ const data = [
     icon: <FaLinkedinIn />, // LinkedIn icon
     link: "https://www.linkedin.com/in/glenntarpey/",
   },
-  { id: 3, icon: <FaWhatsapp />, link: "https://wa.me/+353863997169" },
+  //{ id: 3, icon: <FaWhatsapp />, link: "https://wa.me/+353863997169" },
 ];
 
 export default data;
