afb967524c93a29e26569a01df7f84db603d5b7e
Glenn Tarpey <glenntarpey@gmail.com>
2024-12-19 15:16:09 +0000

    Added github to footer

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -1,7 +1,5 @@
-import {
-  AiOutlineLinkedin
-} from "react-icons/ai";
-import { SiFiverr } from "react-icons/si";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { SiFiverr, SiGithub } from "react-icons/si";
 
 export const links = [
   { id: 1, link: "#", title: "Home" },
@@ -13,5 +11,6 @@ export const links = [
 
 export const socials = [
   { id: 1, link: "https://www.linkedin.com/in/glenntarpey/", icon: <AiOutlineLinkedin /> },
-  { id: 2, link: "https://www.fiverr.com/doctorrobot?up_rollout=true", icon: <SiFiverr /> }
+  { id: 2, link: "https://www.fiverr.com/doctorrobot?up_rollout=true", icon: <SiFiverr /> },
+  { id: 3, link: "https://github.com/Dr-StephenStrange", icon: <SiGithub /> }, 
 ];
